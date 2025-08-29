0e96648ff0d821d850ea8df59cb7247bef313ed1
DanielKruts <djkrutsick@icloud.com>
2025-08-29 11:49:13 -0400

    Meeting for notes on 8/29

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SudokuCube
+
+A Sudoku Cube is a 3D puzzle that combines the principles of Sudoku with the structure of a cube.
+Each face of the cube contains a 3x3 grid, and the goal is to fill in the numbers 1 to 4 on each face such that:
---
+++ SudokuMain.py
@@ -0,0 +1,3 @@
+#Classes - CubeSide Class(Stores matrices, center element(string) 4th element) 6 CubeSide elements for each face
+#          Class Movement - 12 (Cube Side, Column/Row - Binary, Position - Binary, Movement(Up/Down) - Binary, Roate Side, Array of Sides path, Temp side(Maybe0))
+
