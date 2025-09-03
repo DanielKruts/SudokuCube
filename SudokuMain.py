be38a9ead53eaf17cf97f0a93160d6159b08ba85
Jacob Blankenship <jrblanke2@gmail.com>
2025-09-03 11:31:50 -0400

    Changed up formatting, starting class definitions

--- SudokuMain.py
+++ SudokuMain.py
@@ -1,11 +1,23 @@
-#Classes - CubeSide Class(Stores matrices, center element(string) 4th element) 6 CubeSide elements for each face
-#Class Movement - 12 (Cube Side, Column/Row - Binary, Position - Binary, Movement(Up/Down) - Binary, Roate Side, Array of Sides path, Temp side(Maybe0))
-
+'''
+Classes - 
+    CubeSide Class(Stores matrices, center element(string) 4th element) 
+    6 CubeSide elements for each face
+Class Movement - 
+    12 (Cube Side, Column/Row - Binary, Position - Binary, Movement(Up/Down) 
+    Binary, Roate Side, Array of Sides path, Temp side(Maybe0))
+'''
 #Might be removed, but starter class definitions for CubeSide and Movement
 class CubeSide:
-    center = face[1][1]
-
-# Youz a bitch
-    face = [3][3]
+    facevalue = [0,0,0][0,0,0][0,0,0]
+    centervalue = facevalue[1][1]
 
 class Cube:
+    CubeSide
+    
+
+
+
+def initializefunction():
+    Cube.CubeSide.face = 
+
+
