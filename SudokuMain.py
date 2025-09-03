d2b99cc19d2bc60b1718e3e16b6e4d9ea01acf70
Jacob Blankenship <jrblanke2@gmail.com>
2025-09-03 11:55:57 -0400

    Started face initializtion and definitions for some of the classes.

--- SudokuMain.py
+++ SudokuMain.py
@@ -8,16 +8,32 @@ Class Movement -
 '''
 #Might be removed, but starter class definitions for CubeSide and Movement
 class CubeSide:
-    facevalue = [0,0,0][0,0,0][0,0,0]
-    centervalue = facevalue[1][1]
+    facevalue = [[0,0,0],[0,0,0],[0,0,0]]
+    centercolor = ""
+    centervalue = facevalue[1][1] + centercolor
 
 class Cube:
-    CubeSide
-    
+    def __init__(self):
+        self.Front = CubeSide.centercolor("Y")
+        self.Back = CubeSide.centercolor("P")
+        self.Left = CubeSide.centercolor("B")
+        self.Right = CubeSide.centercolor("G")
+        self.Up = CubeSide.centercolor("O")
+        self.Down = CubeSide.centercolor("R")
+
+# class Movement:
 
 
 
 def initializefunction():
-    Cube.CubeSide.face = 
+    Cube.Front.facevalue = [ [9,5,2], [3,8,1], [6,7,4] ]
+    Cube.Back.facevalue = [ [9,5,2], [3,8,1], [6,7,4] ]
+    Cube.Left.facevalue = [ [7,1,8], [2,4,6], [9,3,5] ]
+    Cube.Right.facevalue = [ [4,6,3], [7,5,9], [1,2,8] ]
+    Cube.Up.facevalue = [ [8,1,3], [4,6,7], [2,9,5] ]
+    Cube.Down.facevalue = [ [1,2,8], [5,3,9], [7,4,6] ]
+
+    return Cube.Down.centervalue
+
 
 
