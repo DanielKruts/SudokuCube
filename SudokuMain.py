3fbfe48ee8f76ad88a5f8f95c7abd67b8172aab6
Jacob Blankenship <jrblanke2@gmail.com>
2025-09-03 12:04:46 -0400

    Tryin to print out some stuff

--- SudokuMain.py
+++ SudokuMain.py
@@ -10,7 +10,7 @@ Class Movement -
 class CubeSide:
     facevalue = [[0,0,0],[0,0,0],[0,0,0]]
     centercolor = ""
-    centervalue = facevalue[1][1] + centercolor
+    centervalue = str(facevalue[1][1]) + centercolor
 
 class Cube:
     def __init__(self):
@@ -26,14 +26,13 @@ class Cube:
 
 
 def initializefunction():
-    Cube.Front.facevalue = [ [9,5,2], [3,8,1], [6,7,4] ]
-    Cube.Back.facevalue = [ [9,5,2], [3,8,1], [6,7,4] ]
-    Cube.Left.facevalue = [ [7,1,8], [2,4,6], [9,3,5] ]
-    Cube.Right.facevalue = [ [4,6,3], [7,5,9], [1,2,8] ]
-    Cube.Up.facevalue = [ [8,1,3], [4,6,7], [2,9,5] ]
-    Cube.Down.facevalue = [ [1,2,8], [5,3,9], [7,4,6] ]
+    print("Front center:", Cube.Front.centervalue)
+    print("Front face:", Cube.Front.facevalue)
+    print("Back center:", Cube.Back.centervalue)
+    print("Left face:", Cube.Left.facevalue)
 
-    return Cube.Down.centervalue
+    return Cube
+
 
 
 
